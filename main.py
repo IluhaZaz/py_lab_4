82dab02c41686d992e076a99f3265f142a523421
IluhaZaz <133998226+IluhaZaz@users.noreply.github.com>
2023-11-20 18:43:53 +0300

    Сделаны пункты 9(2 варианта) и 10
    1й вариант 9го пункта нуждается в доработке

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import numpy as np
+import string
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
+from nltk.probability import FreqDist
+import nltk
 from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from pymystem3 import Mystem
 
+trash_list = [x for x in string.punctuation + string.digits]
+trash_list += ["\n", "«", "»"]
 
 PATH_TO_ANNOTATION = "c:\\Users\\Acer\\Documents\\py_lab_2\\annotations_1.csv"
 
@@ -56,12 +64,36 @@ def stats_for_marks(df: pd.DataFrame) -> pd.DataFrame:
 
 def get_hist(df: pd.DataFrame, mark: str) -> pd.DataFrame:
     stemmer = SnowballStemmer("russian")
-    res = {}
+    list = []
+    stopwords_ru = stopwords.words("russian")
     for text in df[df["Тип рецензии"] == mark]['Текст рецензии']:
         tokens = word_tokenize(text)
-        lemmatized_words = [stemmer.stem(word) for word in tokens]
-        list_to_dict(lemmatized_words, res)
-    return pd.DataFrame(res, index=[0])
+        lemmatized_words = [stemmer.stem(word) for word in tokens if word not in stopwords_ru and word not in string.punctuation + "«»–..."]
+        list += lemmatized_words
+    list = nltk.Text(list)
+    return pd.Series(dict(FreqDist(list).most_common(10)))
+
+
+
+def get_hist2(df: pd.DataFrame, mark: str) -> pd.Series:
+    fin_len = 30
+    m = Mystem()
+    d = FreqDist()
+    stopwords_ru = stopwords.words("russian")
+    i = 0
+    for text in df[df["Тип рецензии"] == mark]['Текст рецензии']:
+        i+=1
+        print(i)
+        text = del_trash(text)
+        list = [word for word in m.lemmatize(text) if word not in stopwords_ru + [" ", '  ']]
+        d = merge(d, FreqDist(nltk.Text(list)))
+        d = dict_to_FreqDist(dict(d.most_common(round(fin_len))))
+    return pd.Series(dict(d.most_common(fin_len)))
+
+
+def show_bar(df: pd.Series) -> None:
+    plt.bar(df.index, df.values)
+    plt.show()
 
 
 def list_to_dict(a: list, b: dict) -> dict:
@@ -72,9 +104,34 @@ def list_to_dict(a: list, b: dict) -> dict:
             b[i] =1
     return b
 
+def del_trash(text: str) -> str:
+    res = ''
+    for i in text:
+        if i not in trash_list:
+            res += i
+    return res
 
+def merge(a: FreqDist, b: FreqDist) -> FreqDist:
+    c = FreqDist()
+    for key, value in a.items():
+        c[key] = value
+    for key, value in b.items():
+        if key in a.keys():
+            c[key] += value
+        else:
+            c[key] = value
+    return c
+
+
+def dict_to_FreqDist(a: dict) -> FreqDist:
+    b = FreqDist()
+    for key, value in a.items():
+        b[key] = value
+    return b
 
 if __name__ == "__main__":
     
     df = create_dataframe(PATH_TO_ANNOTATION)
-    print(get_hist(df, "good"))
+    df = get_hist2(df, "bad") 
+    show_bar(df)
+
